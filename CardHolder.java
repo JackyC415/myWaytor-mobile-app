980e50135eeefa242f23f4306cfab9ca2e427871
Jacky Z. Chen <32407740+JackyC415@users.noreply.github.com>
2017-11-14 13:53:20 -0800

    More errors discovered

--- CardHolder.java
+++ CardHolder.java
@@ -4,27 +4,27 @@ public class CardHolder {
 
     private int primaryID;
     private String cardHolderName;
-    private String cardNumber;
-    private int expirationDate;
+    private int cardNumber;
+    private String expirationDate;
     private int cvvNumber;
     private String userAddress;
+    private int userZipCode;
     private String userCity;
     private String userState;
-    private int userZipCode;
     private String userCountry;
 
-    public CardHolder(int primaryID, String cardHolderName, String cardNumber, int expirationDate,
-                            int cvvNumber, String userAddress, String userCity, String userState,
-                            int userZipCode, String userCountry) {
+    public CardHolder(int primaryID, String cardHolderName, int cardNumber, String expirationDate,
+                            int cvvNumber, String userAddress,  int userZipCode,
+                      String userCity, String userState, String userCountry) {
         this.primaryID = primaryID;
         this.cardHolderName = cardHolderName;
         this.cardNumber = cardNumber;
         this.expirationDate = expirationDate;
         this.cvvNumber = cvvNumber;
         this.userAddress = userAddress;
+        this.userZipCode = userZipCode;
         this.userCity = userCity;
         this.userState = userState;
-        this.userZipCode = userZipCode;
         this.userCountry = userCountry;
     }
 
@@ -44,19 +44,19 @@ public class CardHolder {
         this.cardHolderName = cardHolderName;
     }
 
-    public String getCardNumber() {
+    public int getCardNumber() {
         return cardNumber;
     }
 
-    public void setCardNumber(String cardNumber) {
+    public void setCardNumber(int cardNumber) {
         this.cardNumber = cardNumber;
     }
 
-    public int getExpirationDate() {
+    public String getExpirationDate() {
         return expirationDate;
     }
 
-    public void setExpirationDate(Integer expirationDate) {
+    public void setExpirationDate(String expirationDate) {
         this.expirationDate = expirationDate;
     }
 
@@ -64,7 +64,7 @@ public class CardHolder {
         return cvvNumber;
     }
 
-    public void setCvvNumber(Integer cvvNumber) {
+    public void setCvvNumber(int cvvNumber) {
         this.cvvNumber = cvvNumber;
     }
 
@@ -74,6 +74,14 @@ public class CardHolder {
 
     public void setUserAddress(String userAddress) {
         this.userAddress = userAddress;
+    }
+
+    public int getUserZipCode() {
+        return userZipCode;
+    }
+
+    public void setUserZipCode(Integer userZipCode) {
+        this.userZipCode = userZipCode;
     }
 
     public String getUserCity() {
@@ -90,14 +98,6 @@ public class CardHolder {
 
     public void setUserState(String userState) {
         this.userState = userState;
-    }
-
-    public int getUserZipCode() {
-        return userZipCode;
-    }
-
-    public void setUserZipCode(Integer userZipCode) {
-        this.userZipCode = userZipCode;
     }
 
     public String getUserCountry() {
