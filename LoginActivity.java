fb7fbfe775c671ebefc61021cedef5eb1302b670
Jacky Z. Chen <32407740+JackyC415@users.noreply.github.com>
2017-11-14 16:20:13 -0800

    Fixed minor errors

--- LoginActivity.java
+++ LoginActivity.java
@@ -36,26 +36,32 @@ public class LoginActivity extends AppCompatActivity {
             }
 
         });
-         btnLogin.setOnClickListener(new View.OnClickListener() {
+       btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 if (v.getId() == R.id.bLogin) {
-                    String username = inputEmail.getText().toString();
-                    String password = inputPassword.getText().toString();
+                    EditText user = (EditText) findViewById(R.id.etInputUsername);
+                    String username = user.getText().toString();
+                    EditText pass = (EditText) findViewById(R.id.etInputPassword);
+                    String password = pass.getText().toString();
 
                     if (TextUtils.isEmpty(username)) {
                         Toast.makeText(getApplicationContext(), "Enter username!", Toast.LENGTH_SHORT).show();
                         return;
                     }
+
                     if (TextUtils.isEmpty(password)) {
                         Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
                         return;
                     }
-                    String dbPassword = db.LoginAuth(password);
-
-                    if(password.equals(dbPassword)) {
+                    String dbAuth = db.LoginAuth(username);
+                    if (password.equals(dbAuth)) {
                         startActivity(new Intent(LoginActivity.this, DetailedMenuActivity.class));
+                    }
+                    else {
+                        Toast errorMsg = Toast.makeText(LoginActivity.this, "Username and Password not matched!", Toast.LENGTH_SHORT);
+                        errorMsg.show();
                     }
                 }
             }
