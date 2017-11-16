113579075f5bf48a9d15805511d28a7eab76e6fa
Jacky Z. Chen <32407740+JackyC415@users.noreply.github.com>
2017-11-16 01:03:19 -0800

    Added new DB tables for menu features

--- DBController.java
+++ DBController.java
@@ -10,40 +10,75 @@ import java.util.*;
 
 //Database controller; responsible for manipulating all database interaction activities
 //Functions include, but not limited to, insert, update, retrieve, edit and delete
-//Future implementations will include authentications and verifications as well as additional new features
+//SQL queries include: login authentication,retrieve all registration info, total order payment receipt
+//info linked to menu items from all categories: dish, appetizer, drinks & desserts orders, searching
+//for a specific menu item based on its category, filtering based on price range, etc...
 public class DBController extends SQLiteOpenHelper {
 
-    //Database Name
-    private static final String DATABASE_NAME = "myWaytor.db";
     //Database Version
     private static final int DATABASE_VERSION = 1;
 
-    //Database Table names
+    //Database Name
+    private static final String DATABASE_NAME = "myWaytor.db";
+
+    //Database Table Names
     private static final String TABLE_REGISTRATIONS = "Registration";
     private static final String TABLE_CARDHOLDER = "Cardholder";
+    private static final String TABLE_MENU_DISH = "Dish";
+    private static final String TABLE_MENU_APPETIZER = "Appetizer";
+    private static final String TABLE_MENU_DRINKS = "Drinks";
+    private static final String TABLE_MENU_DESSERTS = "Desserts";
+    private static final String TABLE_ORDERS_RECEIPT = "OrdersReceipt";
 
     //Registration Table Column Names
-    private static final String KEY_REGISTRATION_pID = "R_ID";
-    private static final String KEY_REGISTRATION_USERFIRSTNAME = "R_FIRST";
-    private static final String KEY_REGISTRATION_USERLASTNAME = "R_LAST";
-    private static final String KEY_REGISTRATION_USERNAME = "R_USER";
-    private static final String KEY_REGISTRATION_USERPASSWORD = "R_PASS";
-    private static final String KEY_REGISTRATION_USERAGE = "R_AGE";
-    private static final String KEY_REGISTRATION_USERGENDER = "R_GENDER";
-    private static final String KEY_REGISTRATION_USEREMAIL = "R_EMAIL";
+    private static final String KEY_REGISTRATION_pID = "REGISTRATION_pID";
+    private static final String KEY_REGISTRATION_USERFIRSTNAME = "REGISTRATION_FIRST";
+    private static final String KEY_REGISTRATION_USERLASTNAME = "REGISTRATION_LAST";
+    private static final String KEY_REGISTRATION_USERNAME = "REGISTRATION_USER";
+    private static final String KEY_REGISTRATION_USERPASSWORD = "REGISTRATION_PASS";
+    private static final String KEY_REGISTRATION_USERAGE = "REGISTRATION_AGE";
+    private static final String KEY_REGISTRATION_USERGENDER = "REGISTRATION_GENDER";
+    private static final String KEY_REGISTRATION_USEREMAIL = "REGISTRATION_EMAIL";
 
     //CardHolder Table Column Names
-    private static final String KEY_CARD_pID = "C_ID";
-    private static final String KEY_CARD_NAME = "C_NAME";
-    private static final String KEY_CARD_NUMBER = "C_NUMBER";
-    private static final String KEY_CARD_EXPIRATIONDATE = "C_EXPIRATION";
-    private static final String KEY_CARD_CVV = "C_CVV";
-    private static final String KEY_CARD_USERADDRESS = "C_ADDRESS";
-    private static final String KEY_CARD_USERZIPCODE = "C_ZIPCODE";
-    private static final String KEY_CARD_USERCITY = "C_CITY";
-    private static final String KEY_CARD_USERSTATE = "C_STATE";
-    private static final String KEY_CARD_USERCOUNTRY = "C_COUNTRY";
+    private static final String KEY_CARD_pID = "CARD_pID";
+    private static final String KEY_CARD_NAME = "CARD_NAME";
+    private static final String KEY_CARD_NUMBER = "CARD_NUMBER";
+    private static final String KEY_CARD_EXPIRATIONDATE = "CARD_EXPIRATION";
+    private static final String KEY_CARD_CVV = "CARD_CVV";
+    private static final String KEY_CARD_USERADDRESS = "CARD_ADDRESS";
+    private static final String KEY_CARD_USERZIPCODE = "CARD_ZIPCODE";
+    private static final String KEY_CARD_USERCITY = "CARD_CITY";
+    private static final String KEY_CARD_USERSTATE = "CARD_STATE";
+    private static final String KEY_CARD_USERCOUNTRY = "CARD_COUNTRY";
 
+    //Menu Dish Table Column Names
+    private static final String KEY_MENU_DISH_pID = "DISH_pID";
+    private static final String KEY_MENU_DISH_CUSTOMER_NAME = "CUSTOMER_NAME";
+    private static final String KEY_MENU_DISH_NAME = "DISH_NAME";
+    private static final String KEY_MENU_DISH_PRICE = "DISH_PRICE";
+    private static final String KEY_MENU_DISH_QUANTITY = "DISH_QUANTITY";
+
+    //Menu Appetizer Table Column Names
+    private static final String KEY_MENU_APPETIZER_pID = "APPETIZER_pID";
+    private static final String KEY_MENU_APPETIZER_CUSTOMER_NAME = "CUSTOMER_NAME";
+    private static final String KEY_MENU_APPETIZER_NAME = "APPETIZER_NAME";
+    private static final String KEY_MENU_APPETIZER_PRICE = "APPETIZER_PRICE";
+    private static final String KEY_MENU_APPETIZER_QUANTITY = "APPETIZER_QUANTITY";
+
+    //Menu Drinks Table Column Names
+    private static final String KEY_MENU_DRINKS_pID = "D_pID";
+    private static final String KEY_MENU_DRINKS_CUSTOMER_NAME = "CUSTOMER_NAME";
+    private static final String KEY_MENU_DRINKS_NAME = "DRINKS_NAME";
+    private static final String KEY_MENU_DRINKS_PRICE = "DRINKS_PRICE";
+    private static final String KEY_MENU_DRINKS_QUANTITY = "DRINKS_QUANTITY";
+
+    //Menu Desserts Table Column Names
+    private static final String KEY_MENU_DESSERTS_pID = "DESSERTS_pID";
+    private static final String KEY_MENU_DESSERTS_CUSTOMER_NAME = "CUSTOMER_NAME";
+    private static final String KEY_MENU_DESSERTS_NAME = "DESSERTS_NAME";
+    private static final String KEY_MENU_DESSERTS_PRICE = "DESSERTS_PRICE";
+    private static final String KEY_MENU_DESSERTS_QUANTITY = "DESSERTS_QUANTITY";
 
     //Default constructor which generates the database
     public DBController(Context context) {
@@ -51,19 +86,23 @@ public class DBController extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
     }
 
-    //Generating Database Tables for Registration and CardHolder
+    //Generating Database Tables for Registration, CardHolder and Four Menu Categories
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_REGISTRATIONS + " (R_ID INTEGER PRIMARY KEY AUTOINCREMENT, R_FIRST TEXT NOT NULL, R_LAST TEXT NOT NULL, R_USER TEXT UNIQUE, R_PASS TEXT NOT NULL, R_AGE INTEGER NOT NULL, R_GENDER TEXT NOT NULL, R_EMAIL TEXT UNIQUE)");
-        db.execSQL("create table " + TABLE_CARDHOLDER + " (C_ID INTEGER PRIMARY KEY AUTOINCREMENT, C_NAME TEXT NOT NULL, C_NUMBER TEXT NOT NULL, C_EXPIRATION INTEGER NOT NULL, C_CVV INTEGER NOT NULL, C_ADDRESS TEXT NOT NULL, C_ZIPCODE INTEGER NOT NULL, C_CITY TEXT NOT NULL, C_STATE TEXT NOT NULL, C_COUNTRY TEXT NOT NULL)");
+        db.execSQL("create table " + TABLE_REGISTRATIONS + " (REGISTRATION_pID INTEGER PRIMARY KEY AUTOINCREMENT, REGISTRATION_FIRST TEXT NOT NULL, REGISTRATION_LAST TEXT NOT NULL, REGISTRATION_USER TEXT UNIQUE, REGISTRATION_PASS TEXT NOT NULL, REGISTRATION_AGE INTEGER NOT NULL, REGISTRATION_GENDER TEXT NOT NULL, REGISTRATION_EMAIL TEXT UNIQUE)");
+        db.execSQL("create table " + TABLE_CARDHOLDER + " (CARD_pID INTEGER PRIMARY KEY AUTOINCREMENT, CARD_NAME TEXT NOT NULL, CARD_NUMBER INTEGER NOT NULL, CARD_EXPIRATION INTEGER NOT NULL, CARD_CVV INTEGER NOT NULL, CARD_ADDRESS TEXT NOT NULL, CARD_ZIPCODE INTEGER NOT NULL, CARD_CITY TEXT NOT NULL, CARD_STATE TEXT NOT NULL, CARD_COUNTRY TEXT NOT NULL)");
+        db.execSQL("create table " + TABLE_MENU_DISH + "(DISH_pID INTEGER PRIMARY KEY AUTOINCREMENT, CUSTOMER_NAME TEXT NOT NULL, DISH_NAME TEXT NOT NULL, DISH_PRICE DOUBLE NOT NULL, DISH_QUANTITY INTEGER NOT NULL )");
+        db.execSQL("create table " + TABLE_MENU_APPETIZER + "(APPETIZER_pID INTEGER PRIMARY KEY AUTOINCREMENT, CUSTOMER_NAME TEXT NOT NULL, APPETIZER_NAME TEXT NOT NULL, APPETIZER_PRICE DOUBLE NOT NULL, APPETIZER_QUANTITY INTEGER NOT NULL )");
+        db.execSQL("create table " + TABLE_MENU_DRINKS + "(DRINKS_pID INTEGER PRIMARY KEY AUTOINCREMENT, CUSTOMER_NAME TEXT NOT NULL, DRINKS_NAME TEXT NOT NULL, DRINKS_PRICE DOUBLE NOT NULL, DRINKS_QUANTITY INTEGER NOT NULL )");
+        db.execSQL("create table " + TABLE_MENU_DESSERTS + "(DESSERTS_pID INTEGER PRIMARY KEY AUTOINCREMENT, CUSTOMER_NAME TEXT NOT NULL, DESSERTS_NAME TEXT NOT NULL, DESSERTS_PRICE DOUBLE NOT NULL, DESSERTS_QUANTITY INTEGER NOT NULL )");
     }
 
     //Database upgrade routine
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //Drop previous table if exists (Irrelevant for now, since we want to save our previous data)
-        // db.execSQL("DROP TABLE IF EXISTS " + TABLE_REGISTRATIONS);
-        //db.execSQL("DROP TABLE IF EXISTS " + TABLE_CARDHOLDER);
+       //db.execSQL("DROP TABLE IF EXISTS " + TABLE_REGISTRATIONS);
+       //db.execSQL("DROP TABLE IF EXISTS " + TABLE_CARDHOLDER);
         //Regenerate table
         onCreate(db);
     }
@@ -103,6 +142,63 @@ public class DBController extends SQLiteOpenHelper {
         db.insert(TABLE_CARDHOLDER, null, values);
         db.close();
     }
+
+    //Database insert dish menu data routine
+    public void insertMenuDish(MenuDish menuDish) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        //values.put(KEY_MENU_DISH_CUSTOMER_NAME,menuDish.getCustomer_Name());
+        values.put(KEY_MENU_DISH_NAME, menuDish.getDish_Name());
+        values.put(KEY_MENU_DISH_PRICE, menuDish.getDish_Price());
+       // values.put(KEY_MENU_DISH_QUANTITY, menuDish.getDish_Quantity());
+
+        db.insert(TABLE_MENU_DISH, null, values);
+        db.close();
+    }
+
+    //Database insert appetizer menu data routine
+    public void insertMenuAppetizer(MenuAppetizer menuAppetizer) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_MENU_APPETIZER_CUSTOMER_NAME, menuAppetizer.getCustomer_Name());
+        values.put(KEY_MENU_APPETIZER_NAME, menuAppetizer.getAppetizer_Name());
+        values.put(KEY_MENU_APPETIZER_PRICE, menuAppetizer.getAppetizer_Price());
+        values.put(KEY_MENU_APPETIZER_QUANTITY,menuAppetizer.getAppetizer_Quantity());
+
+        db.insert(TABLE_MENU_APPETIZER, null, values);
+        db.close();
+    }
+
+    //Database insert drinks menu data routine
+    public void insertMenuDrinks(MenuDrinks menuDrinks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_MENU_DRINKS_CUSTOMER_NAME,menuDrinks.getDrinks_Name());
+        values.put(KEY_MENU_DRINKS_NAME, menuDrinks.getDrinks_Name());
+        values.put(KEY_MENU_DRINKS_PRICE, menuDrinks.getDrinks_Price());
+        values.put(KEY_MENU_DRINKS_QUANTITY,menuDrinks.getDrinks_Quantity());
+
+        db.insert(TABLE_MENU_DRINKS, null, values);
+        db.close();
+    }
+
+    //Database insert desserts menu data routine
+    public void insertMenuDesserts(MenuDesserts menuDesserts) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_MENU_DESSERTS_CUSTOMER_NAME,menuDesserts.getCustomer_Name());
+        values.put(KEY_MENU_DESSERTS_NAME, menuDesserts.getDesserts_Name());
+        values.put(KEY_MENU_DESSERTS_PRICE, menuDesserts.getDesserts_Price());
+        values.put(KEY_MENU_DESSERTS_QUANTITY,menuDesserts.getDesserts_Quantity());
+
+        db.insert(TABLE_MENU_DESSERTS, null, values);
+        db.close();
+    }
+
 
     //Database retrieve user registration data routine
     public List<Registration> getAllRegistrations() {
@@ -163,7 +259,7 @@ public class DBController extends SQLiteOpenHelper {
         return db.update(TABLE_CARDHOLDER, values, KEY_CARD_pID + "=?",
                 new String[]{String.valueOf(cardholder.getPrimaryID())});
     }
-    
+
     //Database delete user registration data routine
     public void deleteRegistration(Registration registration) {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -191,7 +287,7 @@ public class DBController extends SQLiteOpenHelper {
         String query = "select R_USER, R_PASS from " + TABLE_REGISTRATIONS;
         Cursor cursor = db.rawQuery(query, null);
         String user, pass;
-        pass = "";
+        pass = "try again";
         if (cursor.moveToFirst()) {
             do {
                 user = cursor.getString(0);
