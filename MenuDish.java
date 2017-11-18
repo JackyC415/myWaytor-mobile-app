3f616f21a5517086dc658718364dd863fc9b2cfc
Jacky Z. Chen <32407740+JackyC415@users.noreply.github.com>
2017-11-17 23:33:27 -0800

    Update MenuDish.java

--- MenuDish.java
+++ MenuDish.java
@@ -3,21 +3,21 @@ package com.example.jchen415.mywaytormobileapplication;
 public class MenuDish {
 
     private int dish_ID;
-    private String customer_Name;
     private String dish_Name;
     private double dish_Price;
     private int dish_Quantity;
+    private int dish_Registration_pID;
 
     public MenuDish() {
 
     }
-    public MenuDish(int dish_ID, String customer_Name, String dish_Name, double dish_Price, int dish_Quantity) {
+    public MenuDish(int dish_ID, String dish_Name, double dish_Price, int dish_Quantity, int dish_Registration_pID) {
 
         this.dish_ID = dish_ID;
-        this.customer_Name = customer_Name;
         this.dish_Name = dish_Name;
         this.dish_Price = dish_Price;
         this.dish_Quantity= dish_Quantity;
+        this.dish_Registration_pID = dish_Registration_pID;
     }
 
     public int getDish_ID() {
@@ -26,14 +26,6 @@ public class MenuDish {
 
     public void setDish_ID(int dish_ID) {
         this.dish_ID = dish_ID;
-    }
-
-    public String getCustomer_Name() {
-        return customer_Name;
-    }
-
-    public void setCustomer_Name(String customer_Name) {
-        this.customer_Name = customer_Name;
     }
 
     public String getDish_Name() {
@@ -58,5 +50,13 @@ public class MenuDish {
 
     public void setDish_Quantity(int dish_Quantity) {
         this.dish_Quantity = dish_Quantity;
+    }
+
+    public int getDish_Registration_pID() {
+        return dish_Registration_pID;
+    }
+
+    public void setDish_Registration_pID(int dish_Registration_pID) {
+        this.dish_Registration_pID = dish_Registration_pID;
     }
 }
