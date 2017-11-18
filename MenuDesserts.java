b9597786ee7d9857757d58e6ca1f4edce8b4f6f4
Jacky Z. Chen <32407740+JackyC415@users.noreply.github.com>
2017-11-17 23:33:10 -0800

    Update MenuDesserts.java

--- MenuDesserts.java
+++ MenuDesserts.java
@@ -3,21 +3,21 @@ package com.example.jchen415.mywaytormobileapplication;
 public class MenuDesserts {
 
     private int desserts_ID;
-    private String customer_Name;
     private String desserts_Name;
     private double desserts_Price;
     private int desserts_Quantity;
+    private int desserts_Registration_pID;
 
     public MenuDesserts() {
 
     }
-    public MenuDesserts(int desserts_ID, String customer_Name, String desserts_Name, double desserts_Price, int desserts_Quantity) {
+    public MenuDesserts(int desserts_ID, String desserts_Name, double desserts_Price, int desserts_Quantity, int desserts_Registration_pID) {
 
         this.desserts_ID = desserts_ID;
-        this.customer_Name = customer_Name;
         this.desserts_Name = desserts_Name;
         this.desserts_Price = desserts_Price;
         this.desserts_Quantity= desserts_Quantity;
+        this.desserts_Registration_pID = desserts_Registration_pID;
     }
 
     public int getDesserts_ID() {
@@ -26,14 +26,6 @@ public class MenuDesserts {
 
     public void setDesserts_ID(int desserts_ID) {
         this.desserts_ID = desserts_ID;
-    }
-
-    public String getCustomer_Name() {
-        return customer_Name;
-    }
-
-    public void setCustomer_Name(String customer_Name) {
-        this.customer_Name = customer_Name;
     }
 
     public String getDesserts_Name() {
@@ -58,5 +50,12 @@ public class MenuDesserts {
 
     public void setDesserts_Quantity(int desserts_Quantity) {
         this.desserts_Quantity = desserts_Quantity;
+    }
+    public int getDesserts_Registration_pID() {
+        return desserts_Registration_pID;
+    }
+
+    public void setDesserts_Registration_pID(int desserts_Registration_pID) {
+        this.desserts_Registration_pID = desserts_Registration_pID;
     }
 }
