1a8f1dffc43489aa92378a5b60d94f261c099c6e
Jacky Z. Chen <32407740+JackyC415@users.noreply.github.com>
2017-12-10 13:46:19 -0800

    Added handle card exception

--- DeleteCardActivity.java
+++ DeleteCardActivity.java
@@ -10,20 +10,20 @@ import android.widget.Toast;
 
 public class DeleteCardActivity extends AppCompatActivity {
     DBController db;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_card);
         db = new DBController(this);
-
         final Button bDeleteCard = findViewById(R.id.deleteCard);
 
         bDeleteCard.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                
                 if (db.checkCard() == false) {
                     Toast.makeText(getApplicationContext(), "No Card Exists!", Toast.LENGTH_SHORT).show();
                     return;
-                }
-                else {
+                } else {
                     String PID = db.getCardpID();
                     db.deleteCard(PID);
                     startActivity(new Intent(DeleteCardActivity.this, ManageCardActivity.class));
