dbeb4800d225b181cf7826e227278f212805dd97
Jacky Z. Chen <32407740+JackyC415@users.noreply.github.com>
2017-12-10 13:45:16 -0800

    Update ManageCardActivity.java

--- ManageCardActivity.java
+++ ManageCardActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class ManageCardActivity extends AppCompatActivity {
     DBController db;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_card);
@@ -16,14 +17,12 @@ public class ManageCardActivity extends AppCompatActivity {
 
         final TextView helloTextView = findViewById(R.id.MCgreeting);
         helloTextView.setText(R.string.user_greeting);
-
-
         final TextView MCNum = findViewById(R.id.MCNum);
         final String CardNum = db.getCardpID();
         MCNum.setText(CardNum);
-
         final Button bDeleteCard = findViewById(R.id.deleteCard);
         final Button bAddCard = findViewById(R.id.addCard);
+        
         bAddCard.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(ManageCardActivity.this, CardRegistrationActivity.class));
